3dcf1af40f892affc3af0cfb3e91276e81b6a977
Schlagoo <schlaak.pascal@gmail.com>
2021-06-25 17:56:15 +0200

    merge: ui branch to master

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-
-
 import dash_bootstrap_components as dbc
+from dash_html_components.P import P
 from gradcam import gradcam, extract_predictions
 import plotly.graph_objects as go
 import dash
@@ -11,16 +10,13 @@ from dash.dependencies import Input, Output, State
 
 from utils import base64_to_img, make_img_graph, byte_png_to_img, resize_img
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.title = "Visual Analytics"
 
 server = app.server
 
-# TODO: Show prediction table before images!
-
 app.layout = html.Div([
-    html.H4("1. Upload and predict image"),
+    html.H4("1. Upload an image to get started with Grad-CAM"),
     html.Div([
         html.Div([
             dcc.Upload(
@@ -32,128 +28,23 @@ app.layout = html.Div([
                 className="upload-form",
                 multiple=False
             ),
-            # html.Button("Predict", id="button-predict",
-            #            className="button-main")
         ], className="flexbox"),
-        html.Div(
-            dash_table.DataTable(
-                id='class_table',
-                columns=[
-                    {'name': 'Class', 'id': 'class'},
-                    {'name': 'Confidence', 'id': 'confidence'}
-                ],
-                row_selectable='single',
-                style_cell={'textAlign': 'left', "padding": "0 16px",
-                            'font-family': '"Open Sans", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif'},
-            ), className="space-top"
-        ),
     ], className="container-shadow"),
-    html.H4("2. Select convolution block to compute Grad-CAM",
+    html.H4("2. Perturbate your image to influence the model's prediction",
             className="space-top"),
     html.Div([
         html.Div([
-            html.H6("EfficientNet B0"),
-            html.Img(src=app.get_asset_url(
-                'icon_info.svg'), id="tooltip-model"),
-            dbc.Tooltip(
-                "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-                target="tooltip-model",
-                className="tooltip"
-            ),
-        ], className="header-info"),
-        html.Div([
             html.Div([
-                html.Div(html.Div("Conv3x3", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv1, 3x3", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
-                         className="conv-block flexbox"),
-                html.Div(className="arrow"),
-                html.Div(html.Div("Conv1x1", className="conv-block-text"),
-                         className="conv-block flexbox"),
-            ], className="model-architecture flexbox-row"),
-            html.Div(
-                dcc.Slider(
-                    id="slider_blocks",
-                    min=0,
-                    max=17,
-                    step=None,
-                    marks={
-                        0: 'stem',
-                        1: '1a',
-                        2: '2a',
-                        3: '2b',
-                        4: '3a',
-                        5: '3b',
-                        6: '4a',
-                        7: '4b',
-                        8: '4c',
-                        9: '5a',
-                        10: '5b',
-                        11: '5c',
-                        12: '6a',
-                        13: '6b',
-                        14: '6c',
-                        15: '6d',
-                        16: '7a',
-                        17: 'top'
-                    },
-                    value=17
-                ), className="slider",
-            ),
-            # html.Button("Compute", id="button-gradcam",
-            #           className="button-main"),
-        ], className="flexbox"
-        ),
+                html.Div([
+                    html.H6("Image"),
+                    html.Img(src=app.get_asset_url(
+                        'icon_info.svg'), id="tooltip-image"),
+                    dbc.Tooltip(
+                        "Draw rectangles or other shapes into the image to hide it from the network. Learn more about perturbation below.",
+                        target="tooltip-image",
 
-        html.Div([
-            html.Div([
-                html.H6("Image"),
+                    ),
+                ], className="header-info"),
                 html.Div(dcc.Graph(
                     id="input_graph",
                     figure={},
@@ -167,9 +58,9 @@ app.layout = html.Div([
                     html.Img(src=app.get_asset_url(
                         'icon_info.svg'), id="tooltip-heatmap"),
                     dbc.Tooltip(
-                        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+                        "Here the area of the image that caused the network´s decision is highlighted. Learn more about the Grad-CAM algorithm below.",
                         target="tooltip-heatmap",
-                        className="tooltip"
+
                     ),
                 ], className="header-info"),
                 html.Div(dcc.Graph(
@@ -179,8 +70,140 @@ app.layout = html.Div([
                 ), id="gradcam-div"),
             ], className="container-img"
             ),
-        ], className="flexbox-row space-top"),
+        ], className="flexbox-row"),
+        html.Div([
+            html.Div([
+                html.H6("Predictions"),
+                html.Img(src=app.get_asset_url(
+                    'icon_info.svg'), id="tooltip-prediction"),
+                dbc.Tooltip(
+                    "Select a particular class to calculate Grad-CAM with, otherwise the class with the highest confidence is chosen.",
+                    target="tooltip-prediction",
+
+                ),
+            ], className="header-info"),
+            dash_table.DataTable(
+                id='class_table',
+                columns=[
+                    {'name': 'Class', 'id': 'class'},
+                    {'name': 'Confidence', 'id': 'confidence'}
+                ],
+                row_selectable='single',
+                style_cell={'textAlign': 'left', "padding": "0 16px",
+                            'font-family': '"Open Sans", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif'},
+            )], className="space-top"
+        ),
+        html.Details([
+            html.Summary(
+                html.Div([
+                    html.H6("EfficientNet B0"),
+                    html.Img(src=app.get_asset_url(
+                        'icon_info.svg'), id="tooltip-model"),
+                    dbc.Tooltip(
+                        "Select the feature map of the network that will be used for the Grad-CAM calculation.",
+                        target="tooltip-model",
+                    ),
+                ], className="header-info div-summary"),
+            ),
+            html.Div([
+                html.Div(
+                    dcc.Slider(
+                        id="slider_blocks",
+                        min=0,
+                        max=17,
+                        step=None,
+                        marks={
+                            0: 'stem',
+                            1: '1a',
+                            2: '2a',
+                            3: '2b',
+                            4: '3a',
+                            5: '3b',
+                            6: '4a',
+                            7: '4b',
+                            8: '4c',
+                            9: '5a',
+                            10: '5b',
+                            11: '5c',
+                            12: '6a',
+                            13: '6b',
+                            14: '6c',
+                            15: '6d',
+                            16: '7a',
+                            17: 'top'
+                        },
+                        value=17
+                    ), className="slider",
+                ),
+                html.Div([
+                    html.Div(html.Div("Conv3x3", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv1, 3x3", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 5x5", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("MBConv6, 3x3", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                    html.Div(className="arrow"),
+                    html.Div(html.Div("Conv1x1", className="conv-block-text"),
+                         className="conv-block flexbox"),
+                ], className="model-architecture flexbox-row"),
+            ], className="flexbox"
+            ),
+        ], className="space-top"),
     ], className="container-shadow"),
+    html.Details([
+        html.Summary(html.H4("How does it work?"),
+                     style={"margin-bottom": "12px"}),
+        html.Div([
+            html.H5("Grad-CAM"),
+            html.P("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."),
+            html.H5("Perturbation"),
+            html.P("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+        ], className="container-shadow"),
+    ], className="space-top"),
 ], className="container-main flexbox"
 )
 
@@ -208,7 +231,6 @@ def set_input_img(image_str):
 
 
 @app.callback(Output('gradcam-div', 'children'),
-              #Input("button-gradcam", "n_clicks"),
               Input('input_graph', 'figure'),
               Input('class_table', 'selected_rows'),
               Input('slider_blocks', 'value'),
